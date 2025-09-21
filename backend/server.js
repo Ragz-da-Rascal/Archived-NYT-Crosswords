e9a4350e41eb810d937002c3bdf312bcfda58b9e
Jaron Kelly <jaronkelly0@gmail.com>
2025-09-21 10:16:38 -0400

    ...correctly put the port number and removed the undefined environment variable.

--- backend/server.js
+++ backend/server.js
@@ -88,4 +88,4 @@ app.get('/api/crosswords/:year/random', (req, res) => {
 
 
 
-app.listen(process.env.PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(10000, () => console.log(`Server running on ${PORT}`));
